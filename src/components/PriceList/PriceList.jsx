540b46dd47d2afb9d38841999af694af79314d49
Lilia Lomaka <llomaka80@gmail.com>
2022-08-01 20:00:38 +0300

    Adding price-list background

--- src/components/PriceList/PriceList.jsx
+++ src/components/PriceList/PriceList.jsx
@@ -1,6 +1,9 @@
 import getPriceList from '../../service/priceAPI';
 import { Box, List, ListItem, Typography } from '@mui/material';
 import { useEffect, useState, useId } from 'react';
+import price480 from '../../images/price480.webp';
+import price768 from '../../images/price768.webp';
+import price1280 from '../../images/price1280.webp';
 
 export default function PriceList() {
   const [prices, setPrices] = useState([]);
@@ -22,8 +25,15 @@ export default function PriceList() {
   }, []);
 
   return (
-    <Box>
-      {status==='resolved' && <List>
+    <Box sx={{
+      height: { xs: 826, md: 858, lg: 718 },
+      backgroundColor: '#111319',
+      backgroundImage: { xs: `url(${price480})`, md: `url(${price768})`, lg: `url(${price1280})` },
+      backgroundRepeat: 'no-repeat',
+      backgroundPosition: 'center',
+      backgroundSize: 'cover',
+    }}>
+      {status==='resolved' && <List sx={{color: '#fff'}}>
         {prices.map((item, index) => (
           <ListItem key={id+index}>{item.service}: {item.price}</ListItem>
         ))}
