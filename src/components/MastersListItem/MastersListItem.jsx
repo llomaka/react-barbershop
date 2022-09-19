d85b2028b71bdf2079dcf4e11a3b32a749508fbf
Lilia Lomaka <llomaka80@gmail.com>
2022-09-19 20:12:46 +0300

    Adding hover for master's social icons

--- src/components/MastersListItem/MastersListItem.jsx
+++ src/components/MastersListItem/MastersListItem.jsx
@@ -8,32 +8,32 @@ export default function MastersListItem({ master }) {
   const { image, name, title, instagram, twitter, facebook, linkedin } = master;
 
   return (
-    <Card elevation={0}>
+    <Card elevation={0} sx={{ backgroundColor: '#F7F7F7' }}>
       <CardMedia component='img' image={image} alt={name} />
       <CardContent>
         <Typography variant='h5' component='h3' align='center' mb='8px'>{name}</Typography>
-        <Typography align='center'>{title}</Typography>
+        <Typography align='center' sx={{ color: '#FF6C00' }}>{title}</Typography>
       </CardContent>
       <CardActions>
-        <List sx={{display: 'flex', justifyContent: 'center', alignItems: 'center', gap: '10px', mx: 'auto'}}>
-          <ListItem key='instagram'>
-            <Link href={instagram} target='_blank'>
-              <InstagramIcon sx={{ color: '#303030' }} />
+        <List sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', gap: '10px', mx: 'auto' }}>
+          <ListItem key='instagram' sx={{padding: 0}}>
+            <Link href={instagram} target='_blank' sx={{width: 44, height: 44, backgroundColor: '#FFFFFF', borderRadius: '50%', display: 'flex', alignItems: 'center', justifyContent: 'center', color: '#303030', '&:hover': { color: '#FF6C00' }}}>
+              <InstagramIcon sx={{ color: 'currentColor' }} />
             </Link>
           </ListItem>
-          <ListItem key='twitter'>
-            <Link href={twitter} target='_blank'>
-              <TwitterIcon sx={{ color: '#303030' }} />
+          <ListItem key='twitter' sx={{padding: 0}}>
+            <Link href={twitter} target='_blank' sx={{width: 44, height: 44, backgroundColor: '#FFFFFF', borderRadius: '50%', display: 'flex', alignItems: 'center', justifyContent: 'center', color: '#303030', '&:hover': { color: '#FF6C00' }}}>
+              <TwitterIcon sx={{ color: 'currentColor' }} />
             </Link>
           </ListItem>
-          <ListItem key='facebook'>
-            <Link href={facebook} target='_blank'>
-              <FacebookIcon sx={{ color: '#303030' }} />
+          <ListItem key='facebook' sx={{padding: 0}}>
+            <Link href={facebook} target='_blank' sx={{width: 44, height: 44, backgroundColor: '#FFFFFF', borderRadius: '50%', display: 'flex', alignItems: 'center', justifyContent: 'center', color: '#303030', '&:hover': { color: '#FF6C00' }}}>
+              <FacebookIcon sx={{ color: 'currentColor' }} />
             </Link>
           </ListItem>
-          <ListItem key='linkedin'>
-            <Link href={linkedin} target='_blank'>
-              <LinkedInIcon sx={{ color: '#303030' }} />
+          <ListItem key='linkedin' sx={{padding: 0}}>
+            <Link href={linkedin} target='_blank' sx={{width: 44, height: 44, backgroundColor: '#FFFFFF', borderRadius: '50%', display: 'flex', alignItems: 'center', justifyContent: 'center', color: '#303030', '&:hover': { color: '#FF6C00' }}}>
+              <LinkedInIcon sx={{ color: 'currentColor' }} />
             </Link>
           </ListItem>
         </List>
